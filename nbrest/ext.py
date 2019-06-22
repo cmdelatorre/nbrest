bb5f00682ad913ea35db6ffc502ae33da1c0be29
Javier Mansilla <javier.mansilla@mercadolibre.com>
2019-06-22 15:34:05 -0300

    example handler

--- nbrest/ext.py
+++ nbrest/ext.py
@@ -1,3 +1,10 @@
+from notebook.utils import url_path_join
+from notebook.base.handlers import IPythonHandler
+
+class HelloWorldHandler(IPythonHandler):
+    def get(self):
+        self.finish('Hello, world!')
+
 def load_jupyter_server_extension(nb_server_app):
     """
     Called when the extension is loaded.
@@ -5,5 +12,8 @@ def load_jupyter_server_extension(nb_server_app):
     Args:
         nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
     """
-    print("loadeded...")
-    print('Its a trap')
+    print('its a trap')
+    web_app = nb_server_app.web_app
+    host_pattern = '.*$'
+    route_pattern = url_path_join(web_app.settings['base_url'], '/hello')
+    web_app.add_handlers(host_pattern, [(route_pattern, HelloWorldHandler)])

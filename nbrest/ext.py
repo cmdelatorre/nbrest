7eafb5de26a29b89ef74287ba5250ee189092f00
Carlos Matías de la Torre <carlos.delatorre@mercadolibre.com>
2019-06-22 15:20:06 -0300

    Initial commit

---
+++ nbrest/ext.py
@@ -0,0 +1,8 @@
+def load_jupyter_server_extension(nb_server_app):
+    """
+    Called when the extension is loaded.
+
+    Args:
+        nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
+    """
+    print("loadeded...")

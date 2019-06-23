8e563b40d4d907303078dbde912f786d136bcd9b
Carlos Matías de la Torre <carlos.delatorre@mercadolibre.com>
2019-06-23 09:47:36 -0300

    Code cleansing

--- nbrest/resources.py
+++ nbrest/resources.py
@@ -1,50 +1,30 @@
-import http.server
-import socketserver
-from functools import wraps
 import threading
 
-endpoints = {}
+from flask import Flask
+from flask import request
+from functools import wraps
+
+# Main Flask app where endpoints will be dynamically registered
+app = Flask(__name__)
 
 
 def expose(path):
-    def real_decorator(function):
-        global endpoints
-        print(path, "to endpoints")
-        endpoints[path] = function
+    """Register an endpoint to excute the given function."""
+
+    def register_endpoint(function):
+
         @wraps(function)
-        def wrapper(*args, **kwargs):
-            return function(*args, **kwargs)
-        return wrapper
-    return real_decorator
+        def view(*args, **kwargs):
+            """Target Flask view to handle API requests."""
+            return str(function(**request.args))
+        app.route(path)(view)
+
+        # Return the unmodified decorated function
+        return function
+
+    return register_endpoint
 
 
-
-
-
-
-class MyHandler(http.server.SimpleHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-    def do_GET(self):
-        self._set_headers()
-        print("Do Get Gatooo", self.path, str(endpoints))
-        if self.path in endpoints:
-            self.wfile.write(
-                bytes(
-                    str(endpoints[self.path]()), "utf-8"
-                )
-            )
-        else:
-            super().do_GET()
-
-
-
-
-def runserver(PORT=8911):
-    server = socketserver.TCPServer(("", PORT), MyHandler)
-    thread = threading.Thread(target = server.serve_forever)
-    thread.setdaemon = True
-    thread.start()
+thread = threading.Thread(target = app.run)
+thread.setdaemon = True
+thread.start()
